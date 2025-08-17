88fbf35e3ec11cc6edee1421d60956af06df9a1d
Aidin <122771261+casterwill@users.noreply.github.com>
2025-08-17 11:25:47 +0700

    Add GameCard component and update frontend layout
    Introduces a new GameCard component for displaying game details. Updates the Home page to use a grid layout with multiple GameCard instances and simplifies the page structure. Also serves the frontend static files from the backend server.

---
+++ frontend/components/game-card.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function GameCard() {
+  return (
+    <div className="flex flex-col bg-red-500 h-48 w-30 bg-red-500">
+        <div>game poster</div>
+        <div>game title</div>
+        <div>game price</div>
+    </div>
+  );
+}
