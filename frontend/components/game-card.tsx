1fbee70099ac7fab370a3948b89325def22d947e
Aidin <122771261+casterwill@users.noreply.github.com>
2025-08-18 18:05:02 +0700

    Add Prisma setup and enhance GameCard UI
    Introduces Prisma ORM by adding schema.prisma and related dependencies, updates .gitignore for generated files, and improves the GameCard component UI with better layout and styling.

--- frontend/components/game-card.tsx
+++ frontend/components/game-card.tsx
@@ -2,10 +2,13 @@ import React from "react";
 
 export default function GameCard() {
   return (
-    <div className="flex flex-col bg-red-500 h-48 w-30 bg-red-500">
-        <div>game poster</div>
-        <div>game title</div>
-        <div>game price</div>
+    <div className="flex flex-col h-64 w-48 border rounded-xl shadow-lg bg-gray-800 overflow-hidden">
+        <img src="/game-cover.jpg" alt="Game Cover" className="w-full h-2/3 object-cover"/>
+        <div className="p-2 text-white">
+          <h3 className="text-sm font-bold">Game Title</h3>
+          <p className="text-xs text-gray-300">Genre</p>
+          <h2 className="text-xl font-bold">$16</h2>
+        </div>  
     </div>
   );
-}
+} 
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -6,7 +6,6 @@ export default function Home() {
   return (
     <div className="justify-items-center">
       <div className="grid grid-cols-5 gap-4 ms-8">
-        <div className="bg-red-500 h-20 width-200 height-100">COl1</div>
         <GameCard />
         <GameCard />
         <GameCard />
